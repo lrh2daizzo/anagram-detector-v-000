c7cc50831ded4ad31535202cf5d9a4b41d06f628
Luis Herrera <herrera.lrh@gmail.com>
2018-08-22 22:48:12 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_accessor :name
+
+    def initialize(word)
+      @name = word
+    end
+
+    def match(anagrams)
+      anagrams.select {|word|
+        word.split(//).sort == @name.split(//).sort
+      }
+    end
+
+end
