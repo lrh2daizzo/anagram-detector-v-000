83557add685742201abbd4a2f2ce10152a6a42ea
Luis Herrera <herrera.lrh@gmail.com>
2019-04-04 03:22:48 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,18 +1,17 @@
 # Your code goes here!
 require 'pry'
-
 class Anagram
 
-    attr_accessor :name
+  attr_accessor :name
 
-    def initialize(word)
-      @name = word
-    end
+  def initialize(word)
+    @name = word
+  end
 
-    def match(anagrams)
-      anagrams.select {|word|
-        word.split(//).sort == @name.split(//).sort
-      }
-    end
-
+  def match(anagrams)
+    binding.pry
+    anagrams.select {|word|
+      word.split(//).sort == @name.split(//).sort
+    }
+  end
 end
