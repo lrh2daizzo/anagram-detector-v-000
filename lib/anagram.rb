2cc89b64a624931e222d7542970e0725f017872e
Luis Herrera <herrera.lrh@gmail.com>
2019-11-04 23:29:15 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,14 +2,15 @@
 require 'pry'
 class Anagram
   attr_accessor :word
-
   def initialize(word)
     @word = word
   end
 
   def match(array)
-    array.select do |word| word.split("").sort == @word.split("").sort
+    array.select do |word|
+      word.split("").sort == @word.split("").sort
     end
   end
-  
+
+
 end
