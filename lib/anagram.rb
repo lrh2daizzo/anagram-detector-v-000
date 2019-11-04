6630188d04eb59b0590f4bcfffafcdf6d0ddc621
Luis Herrera <herrera.lrh@gmail.com>
2019-11-04 23:26:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,4 +5,12 @@ class Anagram
   def initialize(word)
     @word = word
   end
+
+  def match(array)
+    array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  end
+
+  
 end
