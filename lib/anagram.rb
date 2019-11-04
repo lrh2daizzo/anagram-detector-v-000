3d50075a54659737ef2087984c9219f043a63515
Luis Herrera <herrera.lrh@gmail.com>
2019-11-04 23:18:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,6 @@
 # Your code goes here!
 require 'pry'
 class Anagram
-  attr_accessor :word
-
-  def initialize(word)
-    @word = word
-  end
-
-  def match(array)
-    array.select do |word| word.split("").sort == @word.split("").sort
-    end
-  end
   
+
 end
